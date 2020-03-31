af3299a3be529fceb3a0499382914c1f68e556a2
Sam Escobar <sescobar8714@gmail.com>
2020-03-31 14:04:59 -0700

    Had some issues trying to figure out a method

--- newsletter.rb
+++ newsletter.rb
@@ -5,7 +5,7 @@ require 'pry'
 #########################
 require "pry"
 CAMPUS = {
-  "name": "DC",
+  "name": "Flatiron",
   "address": "1440 G St NW, Washington, DC 20005",
 }
 DATE = "Nov 20, 2019"
@@ -26,32 +26,45 @@ ARTICLES = [
 #########################
 # Methods to generate the newsletter
 #########################
-
 def calculate_recipients
+  
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
+  SUBSCRIBERS
+
 end
 
-def first_n_articles(number_of_articles
+def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
 end
 
+
 def print_recipients
+  emails = calculate_recipients * ", "
+  
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
 end
 
+
+binding.pry
 def print_one_article(article)
+  article = ARTICLES.each do |obj|
+    if obj == :title && :author && :text 
+     p article
+    end
+  end 
+  
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
 end
-
+ 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
-end
+end 
 
 def format_campus_location(campus)
   "Flatiron #{campus["name"]}"
@@ -80,13 +93,12 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
+
 end
 
-def run
+def run(print_newsletter=3)
   # We want our program to print three articles by default,
   # but we can change that number here
-  print_newsletter("3")
 end
 
 # When we run "ruby newsletter.rb" in the command line,
